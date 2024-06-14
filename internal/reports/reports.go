e073d0185a7c3a594cb0f35c3646255fd1e32df4
Felipe Grilo <felipe.sousa@cloudwalk.io>
2024-06-14 20:46:56 -0300

    implements `GenerateReport`

--- internal/reports/reports.go
+++ internal/reports/reports.go
@@ -1,5 +1,34 @@
 package reports
 
-func GenerateReport() {
-	return
+import (
+	"fmt"
+	"quake_log_parser/pkg/models"
+)
+
+func GenerateReport(match models.Match) string {
+	report := fmt.Sprintf("Match %d:\n", match.ID)
+	report += fmt.Sprintf("Total Kills: %d\n", match.TotalKills)
+	report += "Players:"
+
+	for _, player := range match.Players {
+		report += player.Name + ", "
+	}
+
+	report += "\n Kills:\n"
+
+	for id, kills := range match.Kills {
+		if player, ok := match.Players[id]; ok {
+			report += fmt.Sprintf("%s: %d\n", player.Name, kills)
+		}
+	}
+
+	report += "Kills by Means of Death:\n"
+
+	for mod, count := range match.KillsByMod {
+		report += fmt.Sprintf("%s: %d\n", mod, count)
+	}
+
+	report += "\n"
+
+	return report
 }
