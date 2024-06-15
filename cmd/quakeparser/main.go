636a6842da08bde68b4a0e3f13d43c8237a1e6cc
Felipe Grilo <felipe.sousa@cloudwalk.io>
2024-06-14 21:19:19 -0300

    fix(log): :truck: qgames.log access

--- cmd/quakeparser/main.go
+++ cmd/quakeparser/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"quake_log_parser/internal/app/parser"
 	"quake_log_parser/internal/reports"
 )
 
 func main() {
-	logFilePath := "logs/qgames.log"
-	matches, err := parser.ParseLogFile(logFilePath)
+	currentDir, err := os.Getwd()
 
+	if err != nil {
+		fmt.Printf("Erro ao obter o diretório atual: %v\n", err)
+		return
+	}
+
+	logFilePath := filepath.Join(currentDir, "../../logs/qgames.log")
+
+	if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
+		fmt.Printf("Arquivo de log não encontrado no caminho: %s\n", logFilePath)
+		return
+	}
+
+	matches, err := parser.ParseLogFile(logFilePath)
 	if err != nil {
 		fmt.Printf("Erro ao analisar o arquivo de log: %v\n", err)
 		return
